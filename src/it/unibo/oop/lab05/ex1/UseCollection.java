b25915afee21f010c911ea748f750cdd668d81f0
Giacomo Sirri <giacomo.sirri2@studio.unibo.it>
2021-10-26 15:17:41 +0200

    Implement UseSet.java

--- src/it/unibo/oop/lab05/ex1/UseCollection.java
+++ src/it/unibo/oop/lab05/ex1/UseCollection.java
@@ -1,6 +1,5 @@
 package it.unibo.oop.lab05.ex1;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,6 @@
 package it.unibo.oop.lab05.ex1;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * Example class using {@link Set}.
@@ -30,9 +30,34 @@ public final class UseSet {
          * 4) Removes all those strings whose represented number is divisible by
          * three
          * 
-         * 5) Prints the content of the Set using a for-each costruct
+         * 5) Prints the content of the Set using a for-each construct
          * 
          * 6) Verifies if all the numbers left in the set are even
          */
+    	
+    	final Set<String> set = new TreeSet<>();
+    	for (int i = 1; i <= 20; i++) {
+        	set.add(Integer.toString(i));
+    	}
+    	System.out.println(set);
+    	
+    	Iterator<String> iterator = set.iterator();
+    	while (iterator.hasNext()) {
+    		if (Integer.parseInt(iterator.next()) % 3 == 0) {
+    			iterator.remove();
+    		}
+    	}
+    	
+    	for (String s : set) {
+    		System.out.println(s);
+    	}
+    	
+    	iterator = set.iterator();
+    	int i = 0;
+    	while (iterator.hasNext() && Integer.parseInt(iterator.next()) % 2 != 0) {
+    		i++;
+    	}
+    	System.out.println(i == set.size() ? "set has only even elements" : "set has odd elements too");
+    	
     }
 }
