4aed981546f90723278f9a16a7e9cc470de465f9
Giacomo Sirri <giacomo.sirri2@studio.unibo.it>
2021-10-26 16:16:41 +0200

    Create constructor WarehouseImpl(Set<Product>)

--- src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
+++ src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
@@ -1,5 +1,13 @@
 package it.unibo.oop.lab05.ex3;
 
+import java.util.*;
+
 public class WarehouseImpl {
 
+	private final Set<Product> products;
+	
+	public WarehouseImpl(final Set<Product> products) {
+		this.products = new LinkedHashSet<>(products);
+	}
+	
 }
