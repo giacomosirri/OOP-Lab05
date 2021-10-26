36c946ede86a415521185315083ba6058be67047
Giacomo Sirri <giacomo.sirri2@studio.unibo.it>
2021-10-26 16:49:23 +0200

    Implement ProductImpl.java

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -1,5 +1,33 @@
 package it.unibo.oop.lab05.ex3;
 
-public class ProductImpl {
+public class ProductImpl implements Product {
 
+	private final String name;
+	private double quantity = 0;    // no products if not specified
+	
+	public ProductImpl (final String name, final double quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	public ProductImpl(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getQuantity() {
+		return this.quantity;
+	}
+	
+	private boolean equals(Product p) {
+		return this.getName() == p.getName();
+	}
+	
+	public String toString() {
+		return this.name + ": " + this.quantity;
+	}
+	
 }
