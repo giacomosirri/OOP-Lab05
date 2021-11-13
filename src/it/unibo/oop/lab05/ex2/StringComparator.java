b8746fc8a5bb2e9619a5e9458ff5499b798a8fff
Giacomo Sirri <sirrigiacomo9@gmail.com>
2021-11-13 10:43:33 +0100

    Refine code and add javadoc in ex2

--- src/it/unibo/oop/lab05/ex2/StringComparator.java
+++ src/it/unibo/oop/lab05/ex2/StringComparator.java
@@ -2,24 +2,45 @@ package it.unibo.oop.lab05.ex2;
 
 import java.util.Comparator;
 
+/**
+ * 
+ * Implement a comparison format to compare the numerical (integer) content of two strings
+ */
 public class StringComparator implements Comparator<String> {
 
 	private boolean ascendingOrdering;
 	
+	/**
+	 * @param order
+	 * 			true if the order is ascending.
+	 */
 	public StringComparator(boolean order) {
 		this.ascendingOrdering = order;
 	}
 
+	/**
+	 * Sets the default ordering, which is the ascending order.
+	 */
 	public StringComparator() {
 		this.ascendingOrdering = true;   // the default ordering is the ascending ordering
 	}
 	
+	/**
+	 * Declares how to strings must be compared when they contain integers
+	 * {@inheritDoc}
+	 */
 	public int compare(String a1, String a2) {
 		final int int1 = Integer.parseInt(a1);
 		final int int2 = Integer.parseInt(a2);
 		return this.ascendingOrdering == true ? int1 - int2 : int2 - int1;
 	}
 	
+	/**
+	 * Sets the ordering method.
+	 * 
+	 * @param order
+	 * 			true if the required order is ascending, false otherwise.
+	 */
 	public void setOrdering(boolean order) {
 		this.ascendingOrdering = order;
 	}
--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -4,44 +4,46 @@ import java.util.*;
 
 /**
  * 
+ * A simple use case for a {@link java.util.TreeSet} using a 
+ * {@link it.unibo.oop.lab05.ex2.StringComparator} to insert the string with some order.
  */
 public final class UseSetWithOrder {
 
-    private UseSetWithOrder() {
-    }
+	private UseSetWithOrder() {
+	}
 
-    /**
-     * @param args
-     *            ignored
-     */
-    public static void main(final String[] args) {
-        /*
-         * Write a program which:
-         * 
-         * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
-         * a new Comparator in a separate class.
-         */
-    	final var comp = new StringComparator(true);
-    	final Set<String> set = new TreeSet<String>(comp);
-    	set.add("10");
-    	set.add("92");
-    	set.add("7");
-    	set.add("11");
-    	set.add("50");
-    	System.out.println(set);
-    	
-        /* 
-         * 2) Inserts in the set a hundred strings containing randomly generated
-         * numbers (use Math.random())
-         */ 
-    	for(int i = 0; i < 100; i++) {
-    		Double d = Math.floor(Math.random() * 1000);
-    		set.add(String.valueOf(d.intValue()));
-    	}
-    	
-        /* 
-         * 3) Prints the set, which must be ordered
-         */
-    	System.out.println(set);
-    }
+	/**
+	 * @param args
+	 *            ignored
+	 */
+	public static void main(final String[] args) {
+		/*
+		 * Write a program which:
+		 * 
+		 * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
+		 * a new Comparator in a separate class.
+		 */
+		final var comp = new StringComparator(true);
+		final Set<String> set = new TreeSet<String>(comp);
+		set.add("10");
+		set.add("92");
+		set.add("7");
+		set.add("11");
+		set.add("50");
+		System.out.println(set);
+
+		/* 
+		 * 2) Inserts in the set a hundred strings containing randomly generated
+		 * numbers (use Math.random())
+		 */ 
+		for(int i = 0; i < 100; i++) {
+			Double d = Math.floor(Math.random() * 1000);
+			set.add(String.valueOf(d.intValue()));
+		}
+
+		/* 
+		 * 3) Prints the set, which must be ordered
+		 */
+		System.out.println(set);
+	}
 }
